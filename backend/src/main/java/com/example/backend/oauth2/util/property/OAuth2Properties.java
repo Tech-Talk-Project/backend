72f9e56c9c4048a75eb7a1393011c67156df7423
Hosung <an3735297@naver.com>
2023-11-23 19:37:10 +0900

    feat : 환경 변수로 설정한 oauth2 데이터를 객체로 바꿉니다.

---
+++ backend/src/main/java/com/example/backend/oauth2/util/property/OAuth2Properties.java
@@ -0,0 +1,56 @@
+package com.example.backend.oauth2.util.property;
+
+import com.example.backend.oauth2.OAuth2Provider;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth2")
+@Getter @Setter
+public class OAuth2Properties {
+
+    public OAuth2Property get(OAuth2Provider provider) {
+        switch (provider) {
+            case GITHUB:
+                return github;
+            case NAVER:
+                return naver;
+            case GOOGLE:
+                return google;
+            default:
+                throw new IllegalArgumentException("Invalid OAuth2 Provider");
+        }
+    }
+
+    private Github github;
+    private Naver naver;
+    private Google google;
+
+    @Getter @Setter
+    private static class Github implements OAuth2Property {
+        private String clientId;
+        private String clientSecret;
+        private String redirectUri;
+        private String tokenUri;
+        private String userInfoUri;
+    }
+
+    @Getter @Setter
+    private static class Naver implements OAuth2Property {
+        private String clientId;
+        private String clientSecret;
+        private String redirectUri;
+        private String tokenUri;
+        private String userInfoUri;
+    }
+
+    @Getter @Setter
+    private static class Google implements OAuth2Property{
+        private String clientId;
+        private String clientSecret;
+        private String redirectUri;
+        private String tokenUri;
+        private String userInfoUri;
+    }
+
+}
