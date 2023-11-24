2078c63f2ef42aa4c68709be85fcff5ca8708feb
Hosung <an3735297@naver.com>
2023-11-24 17:56:39 +0900

    refactor, fix : MemberAuthorityRepository
    MemberAuthorityRepository 는 로그인 과정에서만 사용될 것이 예상되고 많은 메서드를 담지 않을 것으로 예상되기 때문에 querydsl, jpa repository 를 다중 상속하는 인터페이스로 만들었습니다.

---
+++ backend/src/main/java/com/example/backend/repository/MemberAuthorityQuerydslImpl.java
@@ -0,0 +1,25 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.member.Authority;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import static com.example.backend.entity.member.QMemberAuthority.*;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberAuthorityQuerydslImpl implements MemberAuthorityQuerydsl{
+    private final JPAQueryFactory query;
+
+    public List<Authority> findAuthoritiesByMemberId(Long memberId) {
+        return query
+                .select(memberAuthority.authority)
+                .from(memberAuthority)
+                .where(memberAuthority.member.id.eq(memberId))
+                .fetch();
+    }
+}
--- backend/src/main/java/com/example/backend/repository/MemberAuthorityRepository.java
+++ backend/src/main/java/com/example/backend/repository/MemberAuthorityRepository.java
@@ -1,40 +1,9 @@
 package com.example.backend.repository;
 
-import com.example.backend.entity.member.Member;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import com.example.backend.entity.member.MemberAuthority;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
-import static com.example.backend.entity.member.QMember.*;
+public interface MemberAuthorityRepository extends JpaRepository<MemberAuthority, Long>, MemberAuthorityQuerydsl {
 
-@Repository
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class MemberAuthorityRepository {
-    private final JPAQueryFactory query;
-
-    public Optional<Member> findByEmailWithAuthorities(String email) {
-        return Optional.ofNullable(
-                query
-                        .selectFrom(member)
-                        .join(member.authorities)
-                        .fetchJoin()
-                        .where(member.email.eq(email))
-                        .fetchFirst()
-        );
-    }
-
-    public Optional<Member> findByIdWithAuthorities(Long id) {
-        return Optional.ofNullable(
-                query
-                        .selectFrom(member)
-                        .join(member.authorities)
-                        .fetchJoin()
-                        .where(member.id.eq(id))
-                        .fetchFirst()
-        );
-    }
 }
