2ac803767758f98b6549ad4c101e17933e6892ce
Hosung <an3735297@pusan.ac.kr>
2023-11-25 19:33:24 +0900

    feat : 로그인, 회원가입을 진행하는 객체입니다.

---
+++ backend/src/main/java/com/example/backend/auth/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.backend.auth.service;
+
+import com.example.backend.auth.dto.LoginSuccessDto;
+import com.example.backend.auth.dto.RegisterSuccessDto;
+import com.example.backend.entity.member.Member;
+import com.example.backend.oauth2.OAuth2Provider;
+import com.example.backend.oauth2.dto.UserProfileDto;
+import com.example.backend.oauth2.service.OAuth2UserProfileService;
+import com.example.backend.security.dto.AtRtDto;
+import com.example.backend.security.service.AtRtCreateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AuthService {
+    private final OAuth2UserProfileService oAuth2UserProfileService;
+    private final MemberRegisterService memberRegisterService;
+    private final AtRtCreateService atRtCreateService;
+
+    @Transactional
+    public LoginSuccessDto login(String code, OAuth2Provider OAuth2Provider) {
+        UserProfileDto userProfile =
+                oAuth2UserProfileService.getUserProfile(code, OAuth2Provider);
+        RegisterSuccessDto registerSuccessDto =
+                memberRegisterService.getOrCreateMember(userProfile, OAuth2Provider);
+        Member member = registerSuccessDto.getMember();
+        boolean firstLogin = registerSuccessDto.isFirstLogin();
+        AtRtDto atRtDto = atRtCreateService.create(member.getId());
+        return new LoginSuccessDto(member, atRtDto, firstLogin);
+    }
+}
