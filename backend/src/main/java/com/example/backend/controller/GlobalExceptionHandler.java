eb2549fa535eed7c0dbef00dbbd9b38dabf02a7f
Hosung <an3735297@naver.com>
2023-11-26 15:44:10 +0900

    refactor : 에러 발생 시 확인할 수 있는 log 추가

--- backend/src/main/java/com/example/backend/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/controller/GlobalExceptionHandler.java
@@ -2,22 +2,27 @@ package com.example.backend.controller;
 
 import com.example.backend.auth.exception.DuplicateOAuth2ProviderException;
 import com.example.backend.security.exception.InvalidJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     //400
     @ExceptionHandler({
             IllegalArgumentException.class,
             IllegalStateException.class,
+            MissingRequestCookieException.class
     })
     @ResponseBody
     public ResponseEntity<Object> handleBadRequestException(Exception ex) {
+        log.error("Bad Request", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -28,6 +33,7 @@ public class GlobalExceptionHandler {
     })
     @ResponseBody
     public ResponseEntity<Object> handleUnAuthorizedException(Exception ex) {
+        log.error("Unauthorized", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
@@ -37,6 +43,7 @@ public class GlobalExceptionHandler {
     })
     @ResponseBody
     public ResponseEntity<Object> handleCustomException(Exception ex) {
+        log.error("Conflict", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
@@ -44,6 +51,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<Object> handleIllegalArgumentException(Exception ex) {
+        log.error("Internal Server Error", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
