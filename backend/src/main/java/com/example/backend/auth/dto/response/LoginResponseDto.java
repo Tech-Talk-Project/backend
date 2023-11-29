c4064d34bd88071cd625eee45082a04c52d21246
Hosung <an3735297@naver.com>
2023-11-29 21:18:26 +0900

    fix : login response dto 스펙 수정

--- backend/src/main/java/com/example/backend/auth/dto/response/LoginResponseDto.java
+++ backend/src/main/java/com/example/backend/auth/dto/response/LoginResponseDto.java
@@ -8,18 +8,12 @@ import lombok.Data;
 @Data
 public class LoginResponseDto {
     private Long memberId;
-    private String name;
-    private String email;
-    private String imageUrl;
     private boolean firstLogin;
     private String accessToken;
     private Long refreshTokenExpirationInMilliSeconds;
 
     public LoginResponseDto(Member member, AtRtDto atRtDto, boolean firstLogin) {
         this.memberId = member.getId();
-        this.name = member.getName();
-        this.email = member.getEmail();
-        this.imageUrl = member.getImageUrl();
         this.firstLogin = firstLogin;
         this.accessToken = atRtDto.getAccessToken();
         this.refreshTokenExpirationInMilliSeconds = atRtDto.getRefreshTokenExpirationInMilliseconds();
@@ -27,9 +21,6 @@ public class LoginResponseDto {
 
     public LoginResponseDto(LoginSuccessDto loginSuccessDto) {
         this.memberId = loginSuccessDto.getMember().getId();
-        this.name = loginSuccessDto.getMember().getName();
-        this.email = loginSuccessDto.getMember().getEmail();
-        this.imageUrl = loginSuccessDto.getMember().getImageUrl();
         this.firstLogin = loginSuccessDto.isFirstLogin();
         this.accessToken = loginSuccessDto.getAtRtDto().getAccessToken();
         this.refreshTokenExpirationInMilliSeconds = loginSuccessDto.getAtRtDto().getRefreshTokenExpirationInMilliseconds();
