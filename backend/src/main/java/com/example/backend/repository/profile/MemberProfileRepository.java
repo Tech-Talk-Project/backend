004d3226c6b152daceb6b8c899abaeb083b9f465
Hosung <an3735297@pusan.ac.kr>
2023-11-30 14:37:10 +0900

    feat : memberId 로 profile 을 조회할 때 사용하는 레파지토리

---
+++ backend/src/main/java/com/example/backend/repository/profile/MemberProfileRepository.java
@@ -0,0 +1,30 @@
+package com.example.backend.repository.profile;
+
+import com.example.backend.entity.profile.Profile;
+import com.example.backend.entity.profile.QLink;
+import com.example.backend.entity.profile.QProfile;
+import com.example.backend.entity.profile.QProfileSkill;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.backend.entity.member.QMember.*;
+import static com.example.backend.entity.profile.QProfile.*;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberProfileRepository {
+    private final JPAQueryFactory query;
+    public Profile findProfileWithAll(Long memberId) {
+        return query
+                .selectFrom(member)
+                .leftJoin(member.profile, profile).fetchJoin()
+                .leftJoin(profile.links, QLink.link).fetchJoin()
+                .leftJoin(profile.profileSkills, QProfileSkill.profileSkill).fetchJoin()
+                .where(member.id.eq(memberId))
+                .fetchOne()
+                .getProfile();
+    }
+}
