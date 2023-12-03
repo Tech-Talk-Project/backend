896c48ff3cadd231b84ae484eae775f8bd8aedde
Hosung <an3735297@pusan.ac.kr>
2023-12-03 13:47:19 +0900

    fix : Profile repo 삭제
    Member 와 Profile 은 영속성 전이와 고아 개체 설정으로 인해 라이프 사이클이 동일 하므로 Profile repo 는 필요 없습니다.

--- backend/src/main/java/com/example/backend/repository/profile/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository.profile;
-
-import com.example.backend.entity.profile.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
