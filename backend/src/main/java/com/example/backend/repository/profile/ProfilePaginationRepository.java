a8433185a0fd1391895fde167e38fc1a3efe51e8
Hosung <an3735297@naver.com>
2023-12-06 20:50:03 +0900

    fix : category 포함 여부 확인 쿼리가 서브쿼리가 되도록 변경

--- backend/src/main/java/com/example/backend/repository/profile/ProfilePaginationRepository.java
+++ backend/src/main/java/com/example/backend/repository/profile/ProfilePaginationRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.repository.profile;
 
 import com.example.backend.entity.profile.*;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -24,15 +25,13 @@ public class ProfilePaginationRepository {
 
 
         // eSKills 를 모두 포함하는 profile 을 찾는다.
-        List<Long> subQuery = query.select(profile.id)
+        JPAQuery<Long> subQuery = query.select(profile.id)
                 .from(profile)
                 .innerJoin(profile.profileSkills, profileSkill)
                 .innerJoin(profileSkill.skill, skill)
                 .where(skill.eSkill.in(eSkills))
                 .groupBy(profile.id)
-                .having(profile.count().goe((long) eSkills.size()))
-                .fetch();
-        System.out.println(subQuery);
+                .having(profile.count().goe((long) eSkills.size()));
 
         // fetchJoin 을 이용해 profileSkill, skill 을 한번에 가져온다.
         // eSkills 를 모두 포함하고 있는 profile 중 커서 기반 페이지네이션을 이용해 limit 만큼 가져온다.
