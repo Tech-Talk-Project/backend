66a2647cecb99d839d2d9238aae51355e2389520
Hosung <an3735297@naver.com>
2024-01-16 14:13:38 +0900

    test : 채팅방 탈퇴 기능 테스트

---
+++ backend/src/test/java/com/example/backend/chat/service/ChatMemberServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.backend.chat.service;
+
+import com.example.backend.chat.domain.ChatMember;
+import com.example.backend.chat.domain.ChatRoom;
+import com.example.backend.chat.repository.ChatMemberRepository;
+import com.example.backend.chat.repository.ChatRoomRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class ChatMemberServiceTest {
+    @Autowired
+    private ChatMemberService chatMemberService;
+
+    @Autowired
+    private ChatRoomManageService chatRoomManageService;
+
+    @Autowired
+    private ChatMemberRepository chatMemberRepository;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    @DisplayName("채팅방에서 탈퇴하기 테스트")
+    @Test
+    void exitChatRoomTest() {
+        // given
+        String title = "테스트 채팅방";
+        ChatMember chatMember1 = new ChatMember(1L);
+        ChatMember chatMember2 = new ChatMember(2L);
+        ChatMember chatMember3 = new ChatMember(3L);
+        chatMemberRepository.save(chatMember1);
+        chatMemberRepository.save(chatMember2);
+        chatMemberRepository.save(chatMember3);
+
+        ChatRoom chatRoom = chatRoomManageService.createChatRoom(title, List.of(1L, 2L, 3L));
+
+        // when
+        chatMemberService.exitChatRoom(chatRoom.getId(), 1L);
+
+        // then
+        // 채팅방에서 탈퇴한 멤버의 joinedChatRooms 에서 채팅방 ID 가 삭제되었는지 확인합니다.
+        ChatMember findChatMember1 = chatMemberRepository.findById(1L);
+        assertThat(findChatMember1.getJoinedChatRooms().size()).isEqualTo(0);
+
+        ChatRoom findChatRoom = chatRoomRepository.findById(chatRoom.getId());
+        assertThat(findChatRoom.getJoinedMemberIds()).containsExactly(2L, 3L);
+    }
+}
