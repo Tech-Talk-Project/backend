7de36f09dcc0a82580fdd2b23eb07d69f1ba4115
Hosung <an3735297@naver.com>
2024-01-16 10:18:31 +0900

    feat : mongodb 기반 채팅 도메인 설계

---
+++ backend/src/main/java/com/example/backend/chat/domain/ChatRoom.java
@@ -0,0 +1,26 @@
+package com.example.backend.chat.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document
+@Getter
+public class ChatRoom {
+    @Id
+    private String id;
+    private String title;
+    private List<Long> joinedMemberIds = new ArrayList<>();
+
+    // 100개까지만 저장하고 그 이전 메세지의 경우 ChatMessage 를 커서 기반으로 페이징합니다.
+    private List<ChatMessage> lastMessages = new ArrayList<>();
+
+    public ChatRoom(String title, List<Long> joinedMemberIds) {
+        this.title = title;
+        this.joinedMemberIds = joinedMemberIds;
+    }
+}
