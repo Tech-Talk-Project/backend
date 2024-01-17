65bd86f9b167924675512d1527798f34971f8f52
Hosung <an3735297@naver.com>
2024-01-17 13:52:50 +0900

    refactor : dto 수정

--- backend/src/main/java/com/example/backend/chat/dto/ChatMessageDto.java
+++ backend/src/main/java/com/example/backend/chat/dto/ChatMessageDto.java
@@ -1,12 +1,21 @@
 package com.example.backend.chat.dto;
 
-import lombok.Data;
+import com.example.backend.chat.domain.ChatMessage;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
+@Getter
+@NoArgsConstructor
 public class ChatMessageDto {
     private Long senderId;
     private Date sendTime;
     private String content;
+
+    public ChatMessageDto(ChatMessage chatMessage) {
+        this.senderId = chatMessage.getSenderId();
+        this.sendTime = chatMessage.getSendTime();
+        this.content = chatMessage.getContent();
+    }
 }
