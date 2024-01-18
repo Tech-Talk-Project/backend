e5d35e68e31544ab3d7f9d6411b476cba4d5ac17
Hosung <an3735297@naver.com>
2024-01-18 14:44:35 +0900

    feat : 채팅방 생성 이후 자동으로 환영 메세지가 보내지도록 변경

--- backend/src/main/java/com/example/backend/chat/service/ChatRoomManageService.java
+++ backend/src/main/java/com/example/backend/chat/service/ChatRoomManageService.java
@@ -4,6 +4,8 @@ import com.example.backend.chat.domain.ChatMember;
 import com.example.backend.chat.domain.ChatRoom;
 import com.example.backend.chat.repository.ChatMemberRepository;
 import com.example.backend.chat.repository.ChatRoomRepository;
+import com.example.backend.repository.member.MemberRepository;
+import com.example.backend.repository.profile.MemberProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,18 @@ import java.util.List;
 public class ChatRoomManageService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMemberRepository chatMemberRepository;
+    private final MemberProfileRepository memberProfileRepository;
+    private final Long ADMIN_ID = -1L;
+
+
     public ChatRoom createChatRoom(String title, List<Long> joinedMemberIds) {
         ChatRoom chatRoom = new ChatRoom(title, joinedMemberIds);
+        chatRoom.getLastMessages().add(
+                new ChatRoom.LastMessage(
+                        ADMIN_ID,
+                        new Date(),
+                        getFirstMessageForMemberName(joinedMemberIds)
+                ));
         chatRoomRepository.save(chatRoom);
 
         // 채팅방 생성 후, 채팅방에 참여한 멤버들의 joinedChatRoomIds 에 채팅방 ID 를 추가합니다.
@@ -28,6 +40,17 @@ public class ChatRoomManageService {
         });
 
         return chatRoom;
+    }
+
+    private String getFirstMessageForMemberName(List<Long> joinedMemberIds) {
+        StringBuilder firstMessage = new StringBuilder();
+        for (Long memberId : joinedMemberIds) {
+            firstMessage.append(memberProfileRepository.findByIdWithProfileInfo(memberId).getProfile().getInfo().getNickname());
+            firstMessage.append(", ");
+        }
+        firstMessage.delete(firstMessage.length() - 2, firstMessage.length());
+        firstMessage.append(" 님이 입장하셨습니다.");
+        return firstMessage.toString();
     }
 
     public void addNewMembers(String chatRoomId, List<Long> newMemberIds) {
