b62500702b36a3e38dd5d74f9b9993462374912c
Hosung <an3735297@naver.com>
2024-01-26 15:14:59 +0900

    feat : 채팅방 ws 구독 요청 시 채팅방에 참여 중인지 유효성 검사

--- backend/src/main/java/com/example/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -1,14 +1,20 @@
 package com.example.backend.config;
 
+import com.example.backend.chat.ws.CustomChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final CustomChannelInterceptor customChannelInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableStompBrokerRelay("/topic");
@@ -20,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(customChannelInterceptor);
     }
 }
