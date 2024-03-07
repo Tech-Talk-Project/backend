8d2079b31fe9fda85c26bf1c017cdb404c4ae064
HOSUNG <an3735297@pusan.ac.kr>
2024-03-08 00:08:13 +0900

    fix : curosr 기반 채팅 메세지 조회 api 버그 수정 2
    시간에 대한 비교 연산이 정상 작동을 확인하였습니다, 이후 curosr 보다 일찍 보내진 메세지들 중 가장 최근의 100개를 보내야 합니다.
    그리고 메세지를 시간 기준 내림차순으로 정렬해야 최근의 것부터 정렬됩니다.
    따라서 정렬 기준을 오름차순에서 내림차순으로 변경하였고  return 시에는 순서를 reverse 하였습니다.

--- backend/src/main/java/com/example/backend/chat/repository/BackupMessagesRepository.java
+++ backend/src/main/java/com/example/backend/chat/repository/BackupMessagesRepository.java
@@ -11,6 +11,8 @@ import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -25,20 +27,31 @@ public class BackupMessagesRepository {
 
     public List<ChatRoom.LastMessage> getChatMessageListAfterCursor(String chatRoomId, Date cursor) {
         MatchOperation matchStage = Aggregation.match(Criteria.where("_id").is(chatRoomId));
+
         Aggregation aggregation = Aggregation.newAggregation(
                 matchStage,
                 Aggregation.unwind("messages"),
                 Aggregation.match(Criteria.where("messages.sendTime").lt(cursor)),
-                Aggregation.sort(Sort.Direction.ASC, "messages.sendTime"),
+                Aggregation.sort(Sort.Direction.DESC, "messages.sendTime"),
                 Aggregation.limit(100),
                 Aggregation.project().and("messages.senderId").as("senderId")
                         .and("messages.sendTime").as("sendTime")
                         .and("messages.content").as("content")
         );
 
-        AggregationResults<ChatRoom.LastMessage> results =
-                mongoTemplate.aggregate(aggregation, BackupMessages.class, ChatRoom.LastMessage.class);
 
-        return results.getMappedResults();
+        List<ChatRoom.LastMessage> results = mongoTemplate
+                                    .aggregate(aggregation, BackupMessages.class, ChatRoom.LastMessage.class)
+                                    .getMappedResults();
+
+        return getReverse(results);
+    }
+
+    private List<ChatRoom.LastMessage> getReverse(List<ChatRoom.LastMessage> results) {
+        List<ChatRoom.LastMessage> reverseResults = new ArrayList<>();
+        for (int i = results.size() - 1; i >= 0; i--) {
+            reverseResults.add(results.get(i));
+        }
+        return reverseResults;
     }
 }
--- backend/src/test/java/com/example/backend/chat/service/ChatMessageListServiceTest.java
+++ backend/src/test/java/com/example/backend/chat/service/ChatMessageListServiceTest.java
@@ -84,6 +84,9 @@ class ChatMessageListServiceTest {
 
         // then
         assertThat(chatMessageListAfterCursor.size()).isEqualTo(100);
+        for (ChatRoom.LastMessage lastMessage : chatMessageListAfterCursor) {
+            System.out.println(lastMessage.getContent());
+        }
     }
 
     @DisplayName("채팅 메시지 리스트 조회 서비스 - 100개 이상 메세지가 보내진 후 - 2번째 페이지 요청")
@@ -94,21 +97,49 @@ class ChatMessageListServiceTest {
                 new UserProfileDto("test", "test@com", "test"),
                 OAuth2Provider.GITHUB);
         ChatRoom chatRoom = chatRoomManageService.createChatRoom("test", List.of(user.getId()));
-        for (int i = 0; i < 75; i++) {
+
+
+        Date cursor1 = new Date();
+        for (int i = 0; i < 50; i++) {
             chatMessageService.send(chatRoom.getId(), user.getId(), "test" + i);
         }
-        Date cursor = new Date();
-        for (int i = 75; i < 150; i++) {
+        Date cursor2 = new Date();
+        for (int i = 50; i < 100; i++) {
             chatMessageService.send(chatRoom.getId(), user.getId(), "test" + i);
         }
+        Date cursor3 = new Date();
+        for (int i = 100; i < 150; i++) {
+            chatMessageService.send(chatRoom.getId(), user.getId(), "test" + i);
+        }
+        Date cursor4 = new Date();
+
+
         // when
-        List<ChatRoom.LastMessage> chatMessageListAfterCursor =
-                chatMessageListService.getChatMessageListAfterCursor(user.getId(), chatRoom.getId(), cursor);
+        List<ChatRoom.LastMessage> chatMessageListAfterCursor1 =
+                chatMessageListService.getChatMessageListAfterCursor(user.getId(), chatRoom.getId(), cursor1);
+        List<ChatRoom.LastMessage> chatMessageListAfterCursor2 =
+                chatMessageListService.getChatMessageListAfterCursor(user.getId(), chatRoom.getId(), cursor2);
+        List<ChatRoom.LastMessage> chatMessageListAfterCursor3 =
+                chatMessageListService.getChatMessageListAfterCursor(user.getId(), chatRoom.getId(), cursor3);
+        List<ChatRoom.LastMessage> chatMessageListAfterCursor4 =
+                chatMessageListService.getChatMessageListAfterCursor(user.getId(), chatRoom.getId(), cursor4);
 
         // then
-        for (ChatRoom.LastMessage lastMessage : chatMessageListAfterCursor) {
-            System.out.println(lastMessage.getContent());
-        }
-        assertThat(chatMessageListAfterCursor.size()).isEqualTo(76);
+        assertThat(chatMessageListAfterCursor1.size()).isEqualTo(1);
+        assertThat(chatMessageListAfterCursor2.size()).isEqualTo(51);
+        assertThat(chatMessageListAfterCursor3.size()).isEqualTo(100);
+        assertThat(chatMessageListAfterCursor4.size()).isEqualTo(100);
+
+        System.out.println(chatMessageListAfterCursor1.get(0).getContent());
+        System.out.println(chatMessageListAfterCursor1.get(chatMessageListAfterCursor1.size() -1).getContent());
+
+        System.out.println(chatMessageListAfterCursor2.get(0).getContent());
+        System.out.println(chatMessageListAfterCursor2.get(chatMessageListAfterCursor2.size() -1).getContent());
+
+        System.out.println(chatMessageListAfterCursor3.get(0).getContent());
+        System.out.println(chatMessageListAfterCursor3.get(chatMessageListAfterCursor3.size() -1).getContent());
+
+        System.out.println(chatMessageListAfterCursor4.get(0).getContent());
+        System.out.println(chatMessageListAfterCursor4.get(chatMessageListAfterCursor4.size() -1).getContent());
     }
 }
