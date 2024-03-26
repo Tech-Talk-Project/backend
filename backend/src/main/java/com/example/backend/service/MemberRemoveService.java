7f018f3296ba1503ade993c07f7fc7d99d8c5fe0
Hosung <an3735297@naver.com>
2024-03-26 17:48:44 +0900

    fix : 회원탈퇴를 진행했을 때 참여 중인 채팅방에서 나가도록 변경

--- backend/src/main/java/com/example/backend/service/MemberRemoveService.java
+++ backend/src/main/java/com/example/backend/service/MemberRemoveService.java
@@ -2,9 +2,9 @@ package com.example.backend.service;
 
 import com.example.backend.chat.domain.ChatMember;
 import com.example.backend.chat.repository.ChatMemberRepository;
+import com.example.backend.chat.service.ChatMemberService;
 import com.example.backend.chat.service.ChatMessageService;
 import com.example.backend.chat.service.MemberId;
-import com.example.backend.repository.member.AuthorityRepository;
 import com.example.backend.repository.member.MemberAuthorityRepository;
 import com.example.backend.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
@@ -17,18 +17,15 @@ import org.springframework.stereotype.Service;
 public class MemberRemoveService {
     private final MemberRepository memberRepository;
     private final ChatMemberRepository chatMemberRepository;
-    private final ChatMessageService chatMessageService;
     private final MemberAuthorityRepository memberAuthorityRepository;
+    private final ChatMemberService chatMemberService;
 
     public void removeMember(Long memberId) {
         ChatMember chatMember = chatMemberRepository.findById(memberId);
-        String memberName = memberRepository.findById(memberId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 사용자입니다.")
-        ).getName();
+        for (ChatMember.JoinedChatRoom joinedChatRoom : chatMember.getJoinedChatRooms()) {
+            chatMemberService.exitChatRoom(joinedChatRoom.getChatRoomId(), memberId);
+        }
         memberAuthorityRepository.deleteAuthoritiesByMemberId(memberId);
         memberRepository.deleteById(memberId);
-        for (ChatMember.JoinedChatRoom joinedChatRoom : chatMember.getJoinedChatRooms()) {
-            chatMessageService.send(joinedChatRoom.getChatRoomId(), MemberId.ADMIN.getValue(), memberName + "님이 나갔습니다.");
-        }
     }
 }
