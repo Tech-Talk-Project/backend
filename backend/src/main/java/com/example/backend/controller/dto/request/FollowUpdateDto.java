aa28d009830de8fd6f420ef6060e5a2d79bc219f
Hosung <an3735297@pusan.ac.kr>
2024-04-01 21:40:56 +0900

    refactor : api 인자를 json 으로 받도록 수정, url 명 수정

---
+++ backend/src/main/java/com/example/backend/controller/dto/request/FollowUpdateDto.java
@@ -0,0 +1,8 @@
+package com.example.backend.controller.dto.request;
+
+import lombok.Data;
+
+@Data
+public class FollowUpdateDto {
+    private Long memberId;
+}
--- backend/src/main/java/com/example/backend/controller/user/follow/FollowController.java
+++ backend/src/main/java/com/example/backend/controller/user/follow/FollowController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller.user.follow;
 
+import com.example.backend.controller.dto.request.FollowUpdateDto;
 import com.example.backend.controller.dto.response.ProfilePaginationResponseDto;
 import com.example.backend.service.follow.FollowService;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,19 @@ import org.springframework.web.bind.annotation.*;
 public class FollowController {
     private final FollowService followService;
 
-    
+    @PostMapping("/add}")
+    public ResponseEntity<String> addFollowing(@RequestBody FollowUpdateDto followUpdateDto) {
+        Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        followService.addFollowing(memberId, followUpdateDto.getMemberId());
+        return ResponseEntity.ok("팔로워가 추가되었습니다.");
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<String> removeFollowing(@RequestBody FollowUpdateDto followUpdateDto) {
+        Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        followService.removeFollowing(memberId, followUpdateDto.getMemberId());
+        return ResponseEntity.ok("팔로워가 제거되었습니다.");
+    }
 
     @GetMapping("/followings")
     public ResponseEntity<ProfilePaginationResponseDto> getFollowingsAfterCursor(
