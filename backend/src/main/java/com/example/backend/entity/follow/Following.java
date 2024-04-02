ab5e68ae4b10e4a8fac51d53555f5162a5550518
Hosung <an3735297@naver.com>
2024-04-02 13:43:47 +0900

    feat : Following 도큐먼트 수정
    Following 도큐먼트에서 팔로윙 하고 있는 사람을 조회할 때 cursor 기반 페이지네이션으로 조회하기 위해서 스펙을 수정했습니다.
    이전에는 id 값만 저장했다면 이제는 id 값과 팔로윙한 시간이 저장되어서 팔로윙한 시간이 빠른 순부터 조회하게 됩니다.

--- backend/src/main/java/com/example/backend/entity/follow/Following.java
+++ backend/src/main/java/com/example/backend/entity/follow/Following.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,9 +14,19 @@ public class Following {
     @Id
     private Long id;
 
-    private Set<Long> followingIds = new HashSet<>();
+    private Set<Person> followingPerson = new HashSet<>();
 
     public Following(Long id) {
         this.id = id;
+    }
+
+    @Getter
+    public static class Person {
+        private Long id;
+        private final Date followingTime = new Date();
+
+        public Person(Long id) {
+            this.id = id;
+        }
     }
 }
