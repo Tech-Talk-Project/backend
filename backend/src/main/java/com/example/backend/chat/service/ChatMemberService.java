e77b5b6a63383e6e1837b2c809738d290f6c6806
Hosung <an3735297@naver.com>
2024-04-07 15:32:59 +0900

    fix : 채팅방 나가기 시 참여중인 채팅방 멤버 수 동기화되지 않았던 에러 해결

--- backend/src/main/java/com/example/backend/chat/service/ChatMemberService.java
+++ backend/src/main/java/com/example/backend/chat/service/ChatMemberService.java
@@ -37,7 +37,7 @@ public class ChatMemberService {
         // 참여하고 있는 채팅방에서 제거
         chatMemberRepository.pullJoinedChatRoom(memberId, chatRoomId);
 
-        Integer memberCount = chatRoomRepository.getMemberCount(chatRoomId);
+        int memberCount = chatRoom.getJoinedMemberIds().size();
         if (memberCount == 0) {
             chatRoomRepository.delete(chatRoom);
             backupMessagesRepository.deleteById(chatRoomId);
