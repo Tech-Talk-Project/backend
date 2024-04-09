512f8669eeccae86fc5fd8146f41ad3e46530031
Hosung <an3735297@pusan.ac.kr>
2024-04-09 17:06:33 +0900

    test : follow test 를 위한 api

--- backend/src/main/java/com/example/backend/controller/TestController.java
+++ backend/src/main/java/com/example/backend/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.backend.controller;
 
+import com.example.backend.entity.member.Member;
+import com.example.backend.repository.member.MemberRepository;
+import com.example.backend.service.follow.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+    private final MemberRepository memberRepository;
+    private final FollowService followService;
+
     @GetMapping("/user")
     public Long user() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -18,5 +27,15 @@ public class TestController {
     @GetMapping("/admin")
     public String admin() {
         return "admin";
+    }
+
+    @GetMapping("/follow-test")
+    public String followTest(@RequestParam Long memberId) {
+        List<Member> all = memberRepository.findAll();
+        for (Member member : all) {
+            if (memberId.equals(member.getId())) continue;
+            followService.addFollowing(memberId, member.getId());
+        }
+        return "follow-test";
     }
 }
