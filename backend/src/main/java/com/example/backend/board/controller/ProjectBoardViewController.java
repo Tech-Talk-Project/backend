1d4b8eba9324fa45c0e11778716b61751c7c8e05
Hosung <an3735297@pusan.ac.kr>
2024-04-17 19:59:54 +0900

    refactor : 프로젝트 상세 페이지 조회 api url 을 restful 하게 변경

--- backend/src/main/java/com/example/backend/board/controller/ProjectBoardViewController.java
+++ backend/src/main/java/com/example/backend/board/controller/ProjectBoardViewController.java
@@ -5,13 +5,11 @@ import com.example.backend.board.service.ProjectBoardViewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/view")
 public class ProjectBoardViewController {
     private final ProjectBoardViewService projectBoardService;
 
