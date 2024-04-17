3890fdf7825cf1651b32d9d1dcc391c41862bbac
Hosung <an3735297@pusan.ac.kr>
2024-04-17 20:08:33 +0900

    feat : api 호출 시점에 log 생성

--- backend/src/main/java/com/example/backend/board/controller/ProjectBoardManageController.java
+++ backend/src/main/java/com/example/backend/board/controller/ProjectBoardManageController.java
@@ -3,6 +3,7 @@ package com.example.backend.board.controller;
 import com.example.backend.board.dto.request.*;
 import com.example.backend.board.service.ProjectBoardManageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/project")
+@Slf4j
 public class ProjectBoardManageController {
     private final ProjectBoardManageService projectBoardManageService;
 
     @PostMapping("/create")
     public ResponseEntity<String> createProject(@RequestBody ProjectCreateRequestDto dto) {
+        log.info("createProject api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.createProject(memberId, dto);
         return ResponseEntity.ok("프로젝트 생성 완료");
@@ -23,6 +26,7 @@ public class ProjectBoardManageController {
     @PostMapping("/update")
     public ResponseEntity<String> updateProject(
             @RequestBody ProjectUpdateRequestDto dto) {
+        log.info("updateProject api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.updateProject(memberId, dto);
         return ResponseEntity.ok("프로젝트 수정 완료");
@@ -31,6 +35,7 @@ public class ProjectBoardManageController {
     @PostMapping("/toggle-recruitment")
     public ResponseEntity<String> toggleRecruitment(
             @RequestBody ProjectUpdateRequestDto dto) {
+        log.info("toggleRecruitment api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.toggleRecruitment(memberId, dto);
         return ResponseEntity.ok("모집 상태 변경 완료");
@@ -39,6 +44,7 @@ public class ProjectBoardManageController {
     @PostMapping("/delete")
     public ResponseEntity<String> deleteProject(
             @RequestBody ProjectRemoveRequestDto dto) {
+        log.info("deleteProject api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.removeProject(memberId, dto);
         return ResponseEntity.ok("프로젝트 삭제 완료");
@@ -47,6 +53,7 @@ public class ProjectBoardManageController {
     @PostMapping("/add-comment")
     public ResponseEntity<String> addComment(
             @RequestBody CommentAddRequestDto dto) {
+        log.info("addComment api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.addComment(memberId, dto);
         return ResponseEntity.ok("댓글이 추가되었습니다.");
@@ -55,6 +62,7 @@ public class ProjectBoardManageController {
     @PostMapping ("/add-like")
     public ResponseEntity<String> addLike(
             @RequestBody LikeRequestDto dto) {
+        log.info("addLike api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.addLike(memberId, dto);
         return ResponseEntity.ok("좋아요가 추가되었습니다.");
@@ -63,6 +71,7 @@ public class ProjectBoardManageController {
     @PostMapping("/remove-like")
     public ResponseEntity<String> removeLike(
             @RequestBody LikeRequestDto dto) {
+        log.info("removeLike api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         projectBoardManageService.removeLike(memberId, dto);
         return ResponseEntity.ok("좋아요가 삭제되었습니다.");
