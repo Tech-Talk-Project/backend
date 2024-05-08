3c4d4da84355b7dd97ebbc71a7c8016318a2eb8b
Hosung <an3735297@pusan.ac.kr>
2024-05-08 12:17:53 +0900

    feat : profile update controller 에 호출 시 log 추가

--- backend/src/main/java/com/example/backend/controller/user/profile/ProfileUpdateController.java
+++ backend/src/main/java/com/example/backend/controller/user/profile/ProfileUpdateController.java
@@ -4,6 +4,7 @@ import com.example.backend.controller.dto.request.*;
 import com.example.backend.service.profile.ProfileUpdateService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/profile/update")
 @RequiredArgsConstructor
+@Slf4j
 public class ProfileUpdateController {
     private final ProfileUpdateService profileUpdateService;
 
     @PostMapping("/info")
     public ResponseEntity<String> updateInfo(@Valid @RequestBody UpdateInfoRequestDto updateInfoRequestDto) {
+        log.info("updateInfo api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         profileUpdateService.updateInfo(memberId, updateInfoRequestDto);
         return ResponseEntity.ok("success");
@@ -26,6 +29,7 @@ public class ProfileUpdateController {
 
     @PostMapping("/introduction")
     public ResponseEntity<String> updateIntroduction(@Valid @RequestBody UpdateIntroductionRequestDto updateIntroductionRequestDto) {
+        log.info("updateIntroduction api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         profileUpdateService.updateIntroduction(memberId, updateIntroductionRequestDto);
         return ResponseEntity.ok("success");
@@ -33,6 +37,7 @@ public class ProfileUpdateController {
 
     @PostMapping("/description")
     public ResponseEntity<String> updateDescription(@Valid @RequestBody UpdateDescRequestDto updateDescRequestDto) {
+        log.info("updateDescription api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         profileUpdateService.updateDescription(memberId, updateDescRequestDto);
         return ResponseEntity.ok("success");
@@ -40,6 +45,7 @@ public class ProfileUpdateController {
 
     @PostMapping("/links")
     public ResponseEntity<String> updateLinks(@Valid @RequestBody UpdateLinksRequestDto updateLinksRequestDto) {
+        log.info("updateLinks api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         profileUpdateService.updateLinks(memberId, updateLinksRequestDto);
         return ResponseEntity.ok("success");
@@ -47,6 +53,7 @@ public class ProfileUpdateController {
 
     @PostMapping("/skills")
     public ResponseEntity<String> updateSkills(@Valid @RequestBody UpdateSkillsRequestDto updateSkillsRequestDto) {
+        log.info("updateSkills api called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         profileUpdateService.updateSkills(memberId, updateSkillsRequestDto);
         return ResponseEntity.ok("success");
