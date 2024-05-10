0c44fb6f36b9e9fdfaed27784181dcb8d19c08b7
Hosung <an3735297@naver.com>
2024-05-10 20:03:15 +0900

    feat : initApp 에서 미리 db 에 authority 를 넣어주는 과정 삭제

--- backend/src/main/java/com/example/backend/repository/member/AuthorityRepository.java
+++ backend/src/main/java/com/example/backend/repository/member/AuthorityRepository.java
@@ -3,7 +3,6 @@ package com.example.backend.repository.member;
 import com.example.backend.entity.member.Authority;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
