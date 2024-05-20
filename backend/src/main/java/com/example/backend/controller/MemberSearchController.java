9ad40e739e4539768a124b5d535954566435052e
Hosung <an3735297@pusan.ac.kr>
2024-05-20 11:30:47 +0900

    feat : member 조회 api 요청 시 log 생성

--- backend/src/main/java/com/example/backend/controller/MemberSearchController.java
+++ backend/src/main/java/com/example/backend/controller/MemberSearchController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.controller.dto.response.MemberSearchResponseDto;
 import com.example.backend.service.member.MemberSearchService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberSearchController {
     private final MemberSearchService memberSearchService;
 
@@ -18,6 +20,7 @@ public class MemberSearchController {
             @RequestParam String email,
             @RequestParam(required = false, defaultValue = "10") int limit
     ) {
+        log.info("/members/search called");
         return ResponseEntity.ok(memberSearchService.findByEmailStartsWithLimit(email, limit));
     }
 }
--- backend/src/main/java/com/example/backend/controller/MemberViewController.java
+++ backend/src/main/java/com/example/backend/controller/MemberViewController.java
@@ -6,6 +6,7 @@ import com.example.backend.service.profile.ProfilePaginationService;
 import com.example.backend.service.profile.ProfileViewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +15,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberViewController {
     private final ProfilePaginationService profilePaginationService;
     private final ProfileViewService profileViewService;
-
 
     @PostMapping("/members")
     public ResponseEntity<ProfileCursorPaginationResponseDto> getProfilesAfterCursor(
             @Valid @RequestBody MembersViewRequestDto membersViewRequestDto
             ) {
+        log.info("/members called");
         String cursor = membersViewRequestDto.getCursor();
         int limit = membersViewRequestDto.getLimit();
         List<String> skills = membersViewRequestDto.getSkills();
@@ -35,6 +37,7 @@ public class MemberViewController {
     public ResponseEntity<AuthProfilePaginationDto> getProfilesAfterCursorWhenLogin(
             @Valid @RequestBody MembersViewRequestDto membersViewRequestDto
     ) {
+        log.info("/user/members called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String cursor = membersViewRequestDto.getCursor();
         int limit = membersViewRequestDto.getLimit();
@@ -48,6 +51,7 @@ public class MemberViewController {
     public ResponseEntity<ProfileResponseDto> getProfile(
             @RequestParam Long selectedMemberId
     ) {
+        log.info("/member called");
         return ResponseEntity.ok(profileViewService.getProfile(selectedMemberId));
     }
 
@@ -55,6 +59,7 @@ public class MemberViewController {
     public ResponseEntity<AuthProfileResponseDto> getProfileWhenLogin(
             @RequestParam Long selectedMemberId
     ) {
+        log.info("/user/member called");
         Long memberId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return ResponseEntity.ok(profileViewService.getSelectedProfileWhenLogin(memberId, selectedMemberId));
     }
