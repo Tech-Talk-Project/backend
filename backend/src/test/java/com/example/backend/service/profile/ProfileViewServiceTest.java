ce9c933b8c9c476c241cb82e34c59494c3b4f626
Hosung <an3735297@pusan.ac.kr>
2024-06-25 10:20:47 +0900

    remove : 사용하지 않는 의존관계 삭제

--- backend/src/test/java/com/example/backend/service/profile/ProfileViewServiceTest.java
+++ backend/src/test/java/com/example/backend/service/profile/ProfileViewServiceTest.java
@@ -10,8 +10,6 @@ import com.example.backend.service.member.MemberCreateService;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
